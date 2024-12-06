cb57561ac995f28476f77d2a17aa45aaf4662fd3
Irabeny <irabeny89@gmail.com>
2024-12-06 20:21:49 +0100

    perf: deserialize formdata of requests to /subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
 
-async fn health_check() -> impl Responder {
+async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}\n", &name)
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
@@ -16,8 +20,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)? //bind error will bubble
     .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 
+const HEALTH_CHECK_PATH: &str = "/health_check";
+const SUBSCRIPTIONS_PATH: &str = "/subscriptions";
+
 // launch server in the background
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -20,10 +23,11 @@ async fn health_check_works() {
 
     // setup http request client
     let client = reqwest::Client::new();
+
     // => Act
     // send request to test the endpoint
     let response = client
-        .get(format!("{}/health_check", address))
+        .get(format!("{address}{HEALTH_CHECK_PATH}"))
         .send()
         .await
         .expect("Failed to execute http request");
@@ -31,4 +35,61 @@ async fn health_check_works() {
     // => Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    // => Arrange
+    // spawn server app in background & extract address
+    let address = spawn_app();
+    // setup http request client
+    let client = reqwest::Client::new();
+
+    // => Act
+    let body = "name=irabeny&email=irabeny89@gmail.com";
+    // send request to test the endpoint
+    let response = client
+        .post(format!("{address}{SUBSCRIPTIONS_PATH}"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute http request");
+
+    // => Assert
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_missing_data() {
+    // => Arrange
+    // spawn server app in background & extract address
+    let address = spawn_app();
+    // setup http request client
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=irabeny", "missing email"),
+        ("email=irabeny89@gmail.com", "missing name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (body, err_msg) in test_cases {
+        // => Act
+        // send request to test the endpoint
+        let response = client
+            .post(format!("{address}{SUBSCRIPTIONS_PATH}"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute http request");
+
+        // => Assert
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "The API did not fail when {}.",
+            err_msg
+        );
+    }
 }
